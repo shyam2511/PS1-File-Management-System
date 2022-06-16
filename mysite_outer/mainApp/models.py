ccfca5bc9eac3c443e4e05ea3b77aa0032f21f3c
Deep Chordia <73215996+DeepC004@users.noreply.github.com>
2022-06-16 14:17:42 +0530

    Update models.py

--- mysite_outer/mainApp/models.py
+++ mysite_outer/mainApp/models.py
@@ -1,75 +1,84 @@
 from django.db import models
-from pymysql import NULL
 
 # Create your models here.
+
 class Customer(models.Model):
-    CustomerId = models.CharField(max_length=30, primary_key=True)
-    EnterpriseName = models.CharField(max_length=50)
+    CustomerId = models.CharField(max_length=30, primary_key=True) #changed floatfield to charfield
+    EnterpriseName = models.CharField(max_length=50, null=True, blank=True)
     CustomerName = models.CharField(max_length=50)
-    EmailId = models.EmailField(max_length=30)
-    MobileNumber = models.CharField(max_length=10)
-    Status = models.CharField(max_length=20)
-    ProfileComplete = models.CharField(max_length=5)
-    KYCStatus = models.CharField(max_length=20)
-    CustomerGroup = models.CharField(max_length=30)
-    CurrentRoyaltyPoints = models.IntegerField()
-    TotalRoyaltyPoints = models.IntegerField() 
+    EmailId = models.EmailField(max_length=30, null=True, blank=True)
+    MobileNumber = models.CharField(max_length=10, null=True, blank=True)
+    Status = models.CharField(max_length=20, null=True, blank=True)
+    ProfileComplete = models.CharField(max_length=5, null=True, blank=True)
+    KYCStatus = models.CharField(max_length=20, null=True, blank=True)
+    CustomerGroup = models.CharField(max_length=30, null=True, blank=True)
+    CurrentLoyaltyPoints = models.FloatField() #changed integer to float
+    TotalLoyaltyPoints = models.FloatField() #changed integer to float
     LastRedemption = models.FloatField()
     TotalRedemption = models.FloatField()
     BonusUponTotal = models.CharField(max_length=30)
-    BirthDate = models.DateField()
-    Gender = models.CharField(max_length=20)
-    Address = models.CharField(max_length=50)
-    PostalCode = models.CharField(max_length=6)
-    Country = models.CharField(max_length=20)
-    State = models.CharField(max_length=30)
-    City = models.CharField(max_length=20)
-    TimeZone = models.CharField(max_length=30)
-    RegisteredDate = models.DateField(auto_now_add=True)
-    UpdatedDate = models.DateField(auto_now=True)
-    VerifiedCustomer = models.CharField(max_length=20)
+    BirthDate = models.CharField(max_length=30, null=True, blank=True)
+    Gender = models.CharField(max_length=20, null=True, blank=True)
+    Address = models.CharField(max_length=100, null=True, blank=True)
+    PostalCode = models.CharField(max_length=10, null=True, blank=True)
+    Country = models.CharField(max_length=20, null=True, blank=True)
+    State = models.CharField(max_length=30, null=True, blank=True)
+    City = models.CharField(max_length=20, null=True, blank=True)
+    TimeZone = models.CharField(max_length=30, null=True, blank=True)
+    RegisteredDate = models.DateTimeField(null=True, blank=True)
+    UpdatedDate = models.DateTimeField(null=True, blank=True)
+    VerifiedCustomer = models.CharField(max_length=20, null=True)
+
+    def str(self):
+        return self.CustomerName
 
 class Bank(models.Model):
-    CustomerId = models.ForeignKey(Customer, on_delete=models.CASCADE, default=NULL)
-    CustomerName = models.CharField(max_length=50)
-    BankName = models.CharField(max_length=30)
-    AccountNumber = models.CharField(max_length=18, primary_key=True)
-    AccountHolderName = models.CharField(max_length=30)
-    IFSCCode = models.CharField(max_length=11)
-    AccountType = models.CharField(max_length=15)
-    BankType = models.CharField(max_length=15)
-    DateCreated = models.DateTimeField(auto_now_add=True)
-    DateModified = models.DateTimeField(auto_now=True)
+    CustomerName = models.CharField(max_length=50, null=True, blank=True)
+    BankName = models.CharField(max_length=50, null=True, blank=True)
+    AccountNumber = models.FloatField(max_length=30, primary_key=True)
+    AccountHolderName = models.CharField(max_length=30, null=True, blank=True)
+    IFSCCode = models.CharField(max_length=11, null=True, blank=True)
+    AccountType = models.CharField(max_length=15, null=True, blank=True)
+    BankType = models.CharField(max_length=15, null=True, blank=True)
+    DateCreated = models.DateTimeField(null=True, blank=True)
+    DateModified = models.DateTimeField(max_length=30, null=True, blank=True)
 
+    def str(self):
+        return self.BankName
 
 class Transaction(models.Model):
-    CustomerId = models.ForeignKey(Customer, on_delete=models.CASCADE, default=NULL)
-    AccountNumber = models.ForeignKey(Bank, on_delete=models.CASCADE, default=NULL)
-    LoyaltyTransaction = models.CharField(max_length=30, primary_key=True)
-    TransactionType = models.CharField(max_length=20)
+    # CustomerId = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    LoyaltyTransactionId = models.CharField(max_length=30)
+    CustomerId = models.CharField(max_length=30) #changed float to char
+    LoyaltyType = models.CharField(max_length=30, null=True, blank=True)
+    TransactionType = models.CharField(max_length=20, null=True, blank=True)
     LoyaltyPoints = models.IntegerField()
-    TransactionStatus = models.CharField(max_length=10)
-    TransactionDetails = models.CharField(max_length=100)
-    Country = models.CharField(max_length=20)
-    State = models.CharField(max_length=30)
-    City = models.CharField(max_length=20)
-    TransactionTime = models.DateTimeField(auto_now_add=True)    
+    TransactionStatus = models.CharField(max_length=10, null=True, blank=True)
+    TransactionDetails = models.CharField(max_length=100, null=True, blank=True)
+    Country = models.CharField(max_length=20, null=True, blank=True)
+    State = models.CharField(max_length=30, null=True, blank=True)
+    City = models.CharField(max_length=20, null=True, blank=True)
+    TransactionTime = models.DateTimeField(primary_key=True)
 
-class Digisol(models.Model):
-    RedemptionID = models.CharField(max_length=30, primary_key=True) #Generate using Hash Functions
-    CustomerId = models.ForeignKey(Customer, on_delete=models.CASCADE, default=NULL)
-    PartnerName = models.CharField(max_length=50)
-    CompanyName = models.CharField(max_length=50)
-    PANNumber = models.CharField(max_length=10)
-    GSTNumber = models.CharField(max_length=15)
-    City = models.CharField(max_length=20)
-    State = models.CharField(max_length=30)
-    VoucherQuality = models.IntegerField()
-    LoyaltyPoints = models.CharField(max_length=20) #loyalty points in Transaction class is int, but output with .00 in summary 
-    ConversionRate = models.CharField(max_length=10)
-    RedemptionValue = models.FloatField()
-    BankName = models.CharField(max_length=30)
-    AccountNumber = models.ForeignKey(Bank, on_delete=models.CASCADE, default=NULL)
-    AccountType = models.CharField(max_length=20)
-    IFSCCode = models.CharField(max_length=11)
-    RedemptionTarget = models.FloatField()
+    def str(self):
+        return self.LoyaltyTransactionId
+
+class Summary(models.Model):
+    Redemption_Transaction_ID = models.AutoField(primary_key=True)
+    Customer_Member_ID = models.FloatField()
+    PartnerName = models.CharField(max_length=50, null=True, blank=True)
+    CompanyName = models.CharField(max_length=50, null=True, blank=True)
+    PAN_Card_Number = models.CharField(max_length=10, null=True, blank=True)
+    GSTNumber = models.CharField(max_length=15, null=True, blank=True)
+    City = models.CharField(max_length=20, null=True, blank=True)
+    State = models.CharField(max_length=30, null=True, blank=True)
+    VoucherQuantity = models.IntegerField(null=True, blank=True)
+    LoyaltyPoints = models.FloatField()
+    ConversionRate = models.FloatField(null=True, blank=True)
+    RedemptionValue = models.FloatField(null=True, blank=True)
+    BankName = models.CharField(max_length=30, null=True, blank=True)
+    BankAccountNumber = models.FloatField(max_length=18)
+    BankAccountType = models.CharField(max_length=20, null=True, blank=True )
+    BankIFSCCode = models.CharField(max_length=11, null=True, blank=True)
+    RedemptionTarget = models.FloatField(null=True, blank=True)
+

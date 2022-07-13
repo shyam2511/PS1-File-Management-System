58e33273a5fb460679ceb27f5589f44da3bf9661
Shyam <67902666+shyam2511@users.noreply.github.com>
2022-07-13 17:47:55 +0530

    individual customer
    meh

--- mysite_outer/mainApp/convertback.py
+++ mysite_outer/mainApp/convertback.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 import mysql.connector
-import os
-from django.conf import settings
 
 
 def converttocsv():
@@ -15,9 +13,23 @@ def converttocsv():
                                   conn)  # here, the 'conn' is the variable that contains your database connection
     # information from step 2
 
-    filepath = os.path.join(settings.BASE_DIR, 'converted_files\output.csv')
-    if os.path.exists(filepath):
-        os.remove(filepath)
+    # filepath = os.path.join(settings.BASE_DIR, 'converted_files\output.xlsx')
+    # if os.path.exists(filepath):
+    # os.remove(filepath)
 
     df = pd.DataFrame(sql_query)
-    df.to_csv(r'.\converted_files\output.csv', index=True)
+    with pd.ExcelWriter(r'.\converted_files\output.xlsx') as writer:
+        df.to_excel(writer, sheet_name='Summary', na_rep='', float_format=None, columns=None,
+                    header=True, index=True, index_label=None, startrow=0, startcol=0, engine=None, merge_cells=True,
+                    encoding=None, inf_rep='inf', verbose=True, freeze_panes=None, storage_options=None)
+        for rows in df.itertuples():
+            print(rows)
+            sheet_name = str(rows[2])
+            df1 = df.copy()
+            df1 = df1.iloc[0:0]
+            df1 = df1.append([rows])
+            df1.to_excel(writer, sheet_name=sheet_name, na_rep='', float_format=None, columns=None,
+                         header=True, index=True, index_label=None, startrow=0, startcol=0, engine=None,
+                         merge_cells=True,
+                         encoding=None, inf_rep='inf', verbose=True, freeze_panes=None, storage_options=None)
+    # df.to_csv(r'.\converted_files\output.csv', index=True)
--- mysite_outer/mainApp/views.py
+++ mysite_outer/mainApp/views.py
@@ -23,112 +23,113 @@ def dashboard(request):
 
 def upload(request):
     if request.method == 'POST':
-        file1 = request.FILES['myfile1']
-        file2 = request.FILES['myfile2']
-        file3 = request.FILES['myfile3']
-        all_files = [file1, file2, file3]
-        print(all_files)
-        for file in all_files:
-            filename = file.name.split('.')[0].lower()
-            print(filename)
-            imported_data = pd.read_csv(file)
-            data = imported_data.dropna(how="all")
-            data.fillna('', inplace=True)
-            data = data.values.tolist()
-            if 'bank' in filename:
-                for row in data:
-                    row[7] = datetime.datetime.strptime(row[7], "%d-%m-%Y %H:%M")
-                    row[8] = datetime.datetime.strptime(row[8], "%d-%m-%Y %H:%M")
-                    value = Bank(
-                        row[0],
-                        row[1],
-                        row[2],
-                        row[3],
-                        row[4],
-                        row[5],
-                        row[6],
-                        row[7],
-                        row[8]
-                    )
-                    value.save()
 
-            elif 'customer' in filename:
-                for row in data:
-                    print(str(row[0]) + 'hello')
-                    row[22] = datetime.datetime.strptime(row[22], "%d-%m-%Y %H:%M")
-                    if len(row[14]) == 0:
-                        row[14] = '2000-01-01'
-                    row[23] = datetime.datetime.strptime(row[23], "%d-%m-%Y %H:%M")
-                    row[0] = str(row[0])
-                    print(type(row[0]))
-                    print(len(row[0]))
-                    value = Customer(
-                        row[0],
-                        row[1],
-                        row[2],
-                        row[3],
-                        row[4],
-                        row[5],
-                        row[6],
-                        row[7],
-                        row[8],
-                        row[9],
-                        row[10],
-                        row[11],
-                        row[12],
-                        row[13],
-                        row[14],
-                        row[15],
-                        row[16],
-                        row[17],
-                        row[18],
-                        row[19],
-                        row[20],
-                        row[21],
-                        row[22],
-                        row[23],
-                        row[24]
-                    )
-                    value.save()
+        if request.FILES != {}:
+            all_files=[]
+            for i in request.FILES:
+                all_files.append(i)
+            print(all_files)
+            for file in all_files:
+                filename = file.name.split('.')[0].lower()
+                print(filename)
+                imported_data = pd.read_csv(file)
+                data = imported_data.dropna(how="all")
+                data.fillna('', inplace=True)
+                data = data.values.tolist()
+                if 'bank' in filename:
+                    for row in data:
+                        row[7] = datetime.datetime.strptime(row[7], "%d-%m-%Y %H:%M")
+                        row[8] = datetime.datetime.strptime(row[8], "%d-%m-%Y %H:%M")
+                        value = Bank(
+                            row[0],
+                            row[1],
+                            row[2],
+                            row[3],
+                            row[4],
+                            row[5],
+                            row[6],
+                            row[7],
+                            row[8]
+                        )
+                        value.save()
 
-            elif 'transaction' in filename:
-                for row in data:
-                    row[10] = datetime.datetime.strptime(row[10], "%d-%m-%Y %H:%M")
-                    print(type(row[10]))
-                    value = Transaction(
-                        row[0],
-                        row[1],
-                        row[2],
-                        row[3],
-                        row[4],
-                        row[5],
-                        row[6],
-                        row[7],
-                        row[8],
-                        row[9],
-                        row[10]
-                    )
-                    value.save()
+                elif 'customer' in filename:
+                    for row in data:
+                        print(str(row[0]) + 'hello')
+                        row[22] = datetime.datetime.strptime(row[22], "%d-%m-%Y %H:%M")
+                        if len(row[14]) == 0:
+                            row[14] = '2000-01-01'
+                        row[23] = datetime.datetime.strptime(row[23], "%d-%m-%Y %H:%M")
+                        row[0] = str(row[0])
+                        print(type(row[0]))
+                        print(len(row[0]))
+                        value = Customer(
+                            row[0],
+                            row[1],
+                            row[2],
+                            row[3],
+                            row[4],
+                            row[5],
+                            row[6],
+                            row[7],
+                            row[8],
+                            row[9],
+                            row[10],
+                            row[11],
+                            row[12],
+                            row[13],
+                            row[14],
+                            row[15],
+                            row[16],
+                            row[17],
+                            row[18],
+                            row[19],
+                            row[20],
+                            row[21],
+                            row[22],
+                            row[23],
+                            row[24]
+                        )
+                        value.save()
 
-    trans = Transaction.objects.all()
-    cursor = connection.cursor()
+                elif 'transaction' in filename:
+                    for row in data:
+                        row[10] = datetime.datetime.strptime(row[10], "%d-%m-%Y %H:%M")
+                        print(type(row[10]))
+                        value = Transaction(
+                            row[0],
+                            row[1],
+                            row[2],
+                            row[3],
+                            row[4],
+                            row[5],
+                            row[6],
+                            row[7],
+                            row[8],
+                            row[9],
+                            row[10]
+                        )
+                        value.save()
 
-    for t in trans:
-        cursor.execute(
-            "Update mainApp_customer set mainApp_customer.CurrentLoyaltyPoints = (%s), "
-            "mainApp_customer.TotalLoyaltyPoints=mainApp_customer.TotalLoyaltyPoints+%s, "
-            "mainApp_customer.UpdatedDate=%s where mainApp_customer.CustomerId = (%s) && "
-            "mainApp_customer.UpdatedDate< (%s)",
-            (t.LoyaltyPoints, t.LoyaltyPoints, t.TransactionTime, t.CustomerId, t.TransactionTime))
+        trans = Transaction.objects.all()
+        cursor = connection.cursor()
 
-        cursor.execute("delete from mainApp_Summary;")
-        cursor.execute(
-            "Insert into mainApp_Summary(Customer_Member_ID, CompanyName, City, State, LoyaltyPoints, BankName, "
-            "BankAccountNumber, BankAccountType, BankIFSCCode) "
-            "SELECT mainApp_Customer.CustomerId, mainApp_Customer.EnterpriseName, mainApp_Customer.City, "
-            "mainApp_Customer.State, mainApp_Customer.TotalLoyaltyPoints,mainApp_Bank.BankName, "
-            "mainApp_Bank.AccountNumber, mainApp_Bank.AccountType, mainApp_Bank.IFSCCode FROM "
-            "mainApp_Customer inner join mainApp_Bank on mainApp_Customer.CustomerName=mainApp_Bank.CustomerName")
+        for t in trans:
+            cursor.execute(
+                "Update mainApp_customer set mainApp_customer.CurrentLoyaltyPoints = (%s), "
+                "mainApp_customer.TotalLoyaltyPoints=mainApp_customer.TotalLoyaltyPoints+%s, "
+                "mainApp_customer.UpdatedDate=%s where mainApp_customer.CustomerId = (%s) && "
+                "mainApp_customer.UpdatedDate< (%s)",
+                (t.LoyaltyPoints, t.LoyaltyPoints, t.TransactionTime, t.CustomerId, t.TransactionTime))
+
+            cursor.execute("delete from mainApp_Summary;")
+            cursor.execute(
+                "Insert into mainApp_Summary(Customer_Member_ID, CompanyName, City, State, LoyaltyPoints, BankName, "
+                "BankAccountNumber, BankAccountType, BankIFSCCode) "
+                "SELECT mainApp_Customer.CustomerId, mainApp_Customer.EnterpriseName, mainApp_Customer.City, "
+                "mainApp_Customer.State, mainApp_Customer.TotalLoyaltyPoints,mainApp_Bank.BankName, "
+                "mainApp_Bank.AccountNumber, mainApp_Bank.AccountType, mainApp_Bank.IFSCCode FROM "
+                "mainApp_Customer inner join mainApp_Bank on mainApp_Customer.CustomerName=mainApp_Bank.CustomerName")
 
     return render(request, 'output.html', {})
 
@@ -136,11 +137,11 @@ def upload(request):
 # Define function to download pdf file using template
 def download(request):
     converttocsv()
-    filename = 'output.csv'
+    filename = 'output.xlsx'
     # Define Django project base directory
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # Define the full file path
-    filepath = BASE_DIR + '\\converted_files\\output.csv'
+    filepath = BASE_DIR + '\\converted_files\\output.xlsx'
     # Open the file for reading content
     path = open(filepath, 'rb')
     # Set the mime type
